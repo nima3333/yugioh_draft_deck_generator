57a4c34d4195d00e27705819ef79a7063830ce4d
Nima Alikhani <nimajaune@gmail.com>
2019-12-01 00:04:03 +0100

    first version

---
+++ readMe.md
@@ -0,0 +1,3 @@
+# Yugioh draft generator
+
+The aim of this script is to created draft decks from a booster for you and your friends.
---
+++ script.py
@@ -0,0 +1,78 @@
+import requests
+import json
+import tkinter
+import tkinter.ttk as ttk
+import re
+import random
+
+r = requests.get('https://db.ygoprodeck.com/api/v5/cardsets.php')
+result = json.loads(r.content.decode('utf-8'))
+sets = [a["Set Name"] for a in result]
+print(len(sets))
+set2 = sets[34]
+print(set2)
+
+def get_random_cards(card_set, n, set_name):
+    #Divide the card set into the common set and other set
+    common_cards = []
+    other_cards = []
+    for card in card_set:
+        for edition in card["card_sets"]:
+            if edition["set_name"]==set_name:
+                if edition["set_rarity"] == "Common":
+                    common_cards.append(card["id"])
+                else:
+                    other_cards.append(card["id"])
+                break
+
+    #Open n boosters
+    cards_list = []
+    for i in range(n):
+        for card_id in random.choices(common_cards, k=8):
+            cards_list.append(card_id)
+        for card_id in random.choices(other_cards, k=1):
+            cards_list.append(card_id)
+    return cards_list
+
+def create_deck_from_cardList(card_set):
+    with open("deck.ydk", "w") as f:  
+        f.write("#main\n")
+        for card in card_set:
+            f.write(card)
+            f.write("\n")
+
+fenetre = tkinter.Tk()
+fenetre.title('Draft deck generator')
+fenetre.configure(background='#3E4149')
+
+set_list = ttk.Combobox(fenetre, values=sets)
+set_list.pack ()
+
+def generate():
+    #Get the name of the booster, and the quantity of boosters
+    set_name = set_list.get()
+    nb_booster = scaller.get()
+
+    #Escape the spaces
+    request_set_name = re.sub(" ", "%20", set_name).lower()
+
+    #API request : get the cards
+    r = requests.get(f'https://db.ygoprodeck.com/api/v5/cardinfo.php?set={request_set_name}')
+    result = json.loads(r.content.decode('utf-8'))
+
+    #Open virtual boosters
+    cards = get_random_cards(result, nb_booster, set_name)
+    create_deck_from_cardList(cards)
+
+scaller = tkinter.Scale(fenetre, orient='horizontal', from_=1, to=20,
+      resolution=1, tickinterval=1, length=350,
+      label='Nombre de booster')
+scaller.pack ()
+
+
+button = tkinter.Button (fenetre, text="Generate", highlightbackground='#3E4149')
+button.config (command = generate)
+button.pack ()
+
+
+fenetre.mainloop()
