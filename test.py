5538561d9d91a7eb1d576c1cc3e2beeb4767373a
Nima Alikhani <nimajaune@gmail.com>
2019-11-30 23:17:17 +0100

    getting the cards of a set

--- test.py
+++ test.py
@@ -12,20 +12,16 @@ sets = [a["Set Name"] for a in result]
 print(len(sets))
 set2 = sets[34]
 print(set2)
-set2 = re.sub(" ", "%20", set2).lower()
-r = requests.get(f'https://db.ygoprodeck.com/api/v5/cardinfo.php?set={set2}')
-result = json.loads(r.content.decode('utf-8'))
-print(result[0])
-cards = [a["name"] for a in result]
-print(len(cards))
 
 def get_n_random_cards(card_set, n):
+    print(len(card_set))
     liste = random.sample(card_set, n)
     return liste
 
 def create_deck_from_cardList(card_set):
-    
-    return()
+    print(card_set)
+    liste = [a["name"] for a in card_set]
+    print(liste)
 
 fenetre = tkinter.Tk()
 fenetre.title('Draft deck generator')
@@ -33,11 +29,16 @@ fenetre.title('Draft deck generator')
 set_list = ttk.Combobox(fenetre, values=sets)
 set_list.pack ()
 
-def print_file () :                     # voir le chapitre sur les événements
-    print(set_list.get())
+def generate () :                     # voir le chapitre sur les événements
+    set_name = set_list.get()
+    set_name = re.sub(" ", "%20", set_name).lower()
+    r = requests.get(f'https://db.ygoprodeck.com/api/v5/cardinfo.php?set={set_name}')
+    result = json.loads(r.content.decode('utf-8'))
+    cards = get_n_random_cards(result, 30)
+    create_deck_from_cardList(cards)
 
 button = tkinter.Button (fenetre, text="Generate")
-button.config (command = print_file)
+button.config (command = generate)
 button.pack (side = tkinter.LEFT)
 
 fenetre.mainloop()
